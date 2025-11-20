d130c8acccc0845eaa071f0716ecd21203f8af83
Olamide <pemzyj123@gmail.com>
2025-11-20 16:42:58 +0100

    database setup successfully

--- server.ts
+++ server.ts
@@ -1,7 +1,25 @@
 import express from "express";
+import type { Request, Response } from "express";
+import pool from "./database/db.js";
+import "dotenv/config"
 
 const app = express();
 
-app.listen(8080, ()=> {
-    console.log(`server is running on port ${8080}`)
+const port = process.env.PORT
+
+app.get("/db-test", async (req: Request, res: Response): Promise<void> => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    res.status(200).json(result.rows);
+    console.log('database connection successful')
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database connection failed" });
+  }
+});
+
+
+app.listen(port, ()=> {
+    console.log(`server is running on port ${port}`)
 });
